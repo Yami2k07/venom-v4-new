58d748c57066875899e46bae769b8249f636f7ea
Yami2k07 <168228799+Yami2k07@users.noreply.github.com>
2024-09-18 18:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7165855621:AAEBN8AgljFzRMndBCrSGWG2OiR_tKSSFVc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Ice")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5265484785")
 
 
 
